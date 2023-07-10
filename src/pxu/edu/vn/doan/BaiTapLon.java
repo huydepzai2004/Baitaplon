18ef40bd35bf2c194c5200933c46e8c9e5b5d392
huydepzai2004 <vovanhuy3102004@gmail.com>
2023-07-10 15:40:15 +0700

    thay doi lan 3

--- src/pxu/edu/vn/doan/BaiTapLon.java
+++ src/pxu/edu/vn/doan/BaiTapLon.java
@@ -3,67 +3,145 @@ package pxu.edu.vn.doan;
 import java.util.Scanner;
 
 public class BaiTapLon {
-
-//ham nhap so luong sinh vien n nguyen duong 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String[] hoten
+		int n = nhapSoLuongSinhVien();
+		System.out.println("n = " + n);
+		// Tao cac mang 1 chieu chua hoten, dToan, dVan, dAnh va DTB
+		String[] hoten = new String[n];
+		double[] dToan = new double[n];
+		double[] dVan = new double[n];
+		double[] dAnh = new double[n];
+		double[] dTB = new double[n];
+		// Goi ham nhap du lieu
+		nhapHoTenVaDiem(hoten, dToan, dVan, dAnh);
+		// Xuat danh sach vua nhap
+		inDSSV(hoten, dToan, dVan, dAnh, dTB);
+		// Goi ham tinh diem trung binh
+		dTB = tinhDTB(dToan, dVan, dAnh);
+		// Xuat danh sach vua nhap
+		inDSSV(hoten, dToan, dVan, dAnh, dTB);
+		sapxepDSSV(hoten, dToan, dVan, dAnh, dTB);
+		timkiemDiem(hoten, dToan, dVan, dAnh, dTB);
+	}
 
-	}
-	
-	//Ham nhap so luong sinh vien N nguyen duong
+	// Ham nhap so luong sinh vien N nguyen duong
 	public static int nhapSoLuongSinhVien() {
-		try {int n;
+		try {
+			int n;
+			Scanner sc = new Scanner(System.in);
+			do {
+				System.out.print("Nhap so luong sinh vien n = ");
+				n = sc.nextInt();
+			} while (n <= 0);
+			return n;
+		} catch (Exception e) {
+			System.out.println("DA XAY RA LOI TRONG QUA TRINH NHAP DU LIEU");
+			return -1;
+		}
+	}
+
+	// Ham nhap ho ten, diem Toan, Van Anh
+	public static void nhapHoTenVaDiem(String[] hoten, double[] dToan, double[] dVan, double[] dAnh) {
+		System.out.println("*************************************************************");
+		try {
+			Scanner sc = new Scanner(System.in);
+			for (int i = 0; i < hoten.length; i++) {
+				System.out.println("Nhap du lieu cho sinh vien thu " + (i + 1));
+				System.out.print("Ho ten: ");
+				hoten[i] = sc.next();
+				System.out.print("Diem Toan: ");
+				dToan[i] = sc.nextDouble();
+				System.out.print("Diem Van: ");
+				dVan[i] = sc.nextDouble();
+				System.out.print("Diem Anh: ");
+				dAnh[i] = sc.nextDouble();
+
+			}
+		} catch (Exception e) {
+			System.out.println("DA XAY RA LOI TRONG QUA TRINH NHAP DU LIEU");
+		}
+		System.out.println("*************************************************************");
+	}
+
+	// Ham tinh diem trung binh
+	public static double[] tinhDTB(double[] dToan, double[] dVan, double[] dAnh) {
+		double[] dtb = new double[dToan.length];
+		for (int i = 0; i < dtb.length; i++) {
+			dtb[i] = Math.round(((dToan[i] + dVan[i] + dAnh[i]) / 3) * 100.0) / 100.0;
+		}
+		return dtb;
+	}
+
+	// Ham sap xep sinh vien theo chieu giam dan cua diem trung binh
+	public static void sapxepDSSV(String[] hoten, double[] dToan, double[] dVan, double[] dAnh, double[] dTB) {
+
+		for (int i = 0; i < dTB.length - 1; i++) {
+			for (int j = i + 1; j < dTB.length; j++) {
+				if (dTB[i] < dTB[j]) {
+					double tam = dTB[i];
+					dTB[i] = dTB[j];
+					dTB[j] = tam;
+					tam = dToan[i];
+					dToan[i] = dToan[j];
+					dToan[j] = tam;
+					//mon van
+					tam=dVan[i];
+					dVan[i]=dVan[j];
+					dVan[j]=tam;
+					//d anh
+					tam=dAnh[i];
+					dAnh[i]=dAnh[j];
+					dAnh[j]=tam;
+					//ho ten
+					String tam2 =hoten[i];
+					hoten[i]=hoten[j];
+					hoten[j]=tam2;
+				}
+
+			}
+		}
+	}
+
+	// Ham in danh sach sinh vien ra man hinh
+	public static void inDSSV(String[] hoten, double[] dToan, double[] dVan, double[] dAnh, double[] dTB) {
+		System.out.println("*************************************************************");
+		System.out.println("\t\t\t DANH SAH SINH VIEN");
+		for (int i = 0; i < hoten.length; i++) {
+			System.out.println(
+					hoten[i] + "; Toan: " + dToan[i] + "; Van: " + dVan[i] + "; Anh: " + dAnh[i] + "; DTB: " + dTB[i]);
+		}
+		System.out.println("*************************************************************");
+	}
+
+	// Ham tim kiem sinh vien theo diem mon Toan
+	public static void timkiemDiem(String[] hoten, double[] dToan, double[] dVan, double[] dAnh, double[] dTB) {
+		int count = 0;
 		Scanner sc = new Scanner(System.in);
-		do {
-			System.out.print("Nhap so luong sinh vien n = ");
-			n = sc.nextInt();
-		}while( n <= 0);
-		return n;
-	}catch(Exception e) {
-		System.out.println("DA XAY RA LOI TRONG QUA TRINH NHAP DU LIEU");
-		return -1;
+		double diemcantim;
+		try {
+			do {
+				System.out.print("Diem Toan can tim: ");
+				diemcantim = sc.nextDouble();
+			}while(diemcantim < 0.0 || diemcantim > 10.0);
+			
+			for (int i = 0; i < dToan.length; i++) {
+				if (dToan[i] == diemcantim) {
+					System.out.println(hoten[i] + "; Toan: " + dToan[i] + "; Van: " + dVan[i] + "; Anh: " + dAnh[i]
+							+ "; DTB: " + dTB[i]);
+					count = count + 1;
+				}
+			}
+			if(count == 0) {
+				System.out.println("KHONG TIM THAY");
+			}
+		}catch(Exception e){
+			System.out.println("DA XAY RA LOI TRONG QUA TRINH NHAP DU LIEU");
+		}	
 	}
-}
-	//Ham nhap ho ten, diem Toan, Van Anh
-	public static void nhapHoTenVaDiem(String[] hoten,
-										double[] dToan,
-										double[] dVan,
-										double[] dAnh) {
-		
-	}
-	//Ham tinh diem trung binh
-	
-	//Ham sap xep sinh  vien theo chieu giam dan cua diem trung binh
-	public static void sapxepDSSV(String[] hoten,
-										double[] dToan,
-										double[] dVan,
-										double[] dAnh,
-										double[] dTB) {
-		
-	}
-	//Ham in danh sach sinh vien ra man hinh
-	public static void inDSSV(String[] hoten,
-										double[] dToan,
-										double[] dVan,
-										double[] dAnh,
-										double[] dTB) {
-		
-	}
-	//Ham tim kiem sinh vien theo diem mon Toan
-	public static void timkiemDiem(String[] hoten,
-										double[] dToan,
-										double[] dVan,
-										double[] dAnh,
-										double[] dTB) {
-		
-	}
-	//Ham tim kiem sinh vien theo ho ten
-	public static void timHoTen(String[] hoten,
-										double[] dToan,
-										double[] dVan,
-										double[] dAnh,
-										double[] dTB) {
-		
+
+	// Ham tim kiem sinh vien theo ho ten
+	public static void timHoTen(String[] hoten, double[] dToan, double[] dVan, double[] dAnh, double[] dTB) {
+
 	}
 }
